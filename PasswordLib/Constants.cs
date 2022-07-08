e779953b787828861c658812d6e7164ecc0491ff
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2022-07-08 08:19:12 +0700

    Update project

---
+++ PasswordLib/Constants.cs
@@ -0,0 +1,32 @@
+﻿namespace PasswordLib
+{
+    public class Constants
+    {
+        public static readonly char[] SPECIAL_CHARACTERS =
+        {
+            '!','@','#','$','%','^','&','*','(',')',
+            '-','_','"','\'','+',',','.','/',':',';',
+            '<','=','>','?','\\','[',']','{','}','~',
+            '`'
+        };
+
+        public static readonly char[] UPPER_CHARACTERS =
+        {
+            'A','B','C','D','E','F','G','H','I','J','K','L',
+            'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+        };
+
+        public static readonly char[] LOWER_CHARACTERS =
+        {
+            'a','b','c','d','e','f','g','h','i','j','k','l',
+            'm','n','o','p','q','r','s','t','u','v','w','x','y','z'
+        };
+
+        public static readonly char[] NUMBERS =
+        {
+            '0','1','2','3','4','5','6','7','8','9'
+        };
+
+        public static readonly 
+    }
+}
